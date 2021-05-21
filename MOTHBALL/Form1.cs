018f644780b70d1cf6fe637cd170022435b34619
8nrivers <8nrivers@hbhs.school.nz>
2021-05-21 15:14:12 +1200

    Changed form name and added icon

--- MOTHBALL/Form1.cs
+++ MOTHBALL/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MOTHBALL
 {
-    public partial class Form1 : Form
+    public partial class frmMenu : Form
     {
-        public Form1()
+        public frmMenu()
         {
             InitializeComponent();
         }
--- MOTHBALL/Program.cs
+++ MOTHBALL/Program.cs
@@ -16,7 +16,7 @@ namespace MOTHBALL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMenu());
         }
     }
 }
